6725ee59d3569604d30fc20bed87eb9c9284854d
Rom Bass <rombass28@gmail.com>
2020-06-15 21:00:34 +0300

    create user with async await

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -8,11 +8,21 @@ class Users {
         .catch(err => res.status(500).json(err));
     }
 
-    create(req, res) {
+    // create(req, res) {
+    //     const newUser = new User(req.body);
+    //     newUser.save()
+    //         .then(user => res.status(201).json(user))
+    //         .catch(err => res.status(400).json(err));
+    // }
+
+    async create(req, res) {
         const newUser = new User(req.body);
-        newUser.save()
-            .then(user => res.status(201).json(user))
-            .catch(err => res.status(400).json(err));
+        try{
+            const createdUser = await newUser.save();
+            res.status(201).json(createdUser);
+        } catch (err) {
+            res.status(400).json(err)
+        }
     }
 }
 
